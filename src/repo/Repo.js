ba4197809a49c9b02de00c96567d6ab0984edcf1
Praveenkumar Ganesan <47271986+praveenkumar1610@users.noreply.github.com>
2021-01-11 15:49:55 +0530

    withPagination

--- src/repo/Repo.js
+++ src/repo/Repo.js
@@ -7,39 +7,60 @@ export default function Repo(props) {
     const [endIndex,setEndIndex]=useState(10);
     const [page,setPage]=useState(1);
     const totalNumberRepo=repoArray.length;
-    const viewLimit=10;
-    const totalPages=Math.ceil(totalNumberRepo/viewLimit)
+    const totalPages=Math.ceil(totalNumberRepo/10)
     
+    function pages(){ 
+        const listPage=[]
+        for(let i=1;i<=totalPages;i++){
+            listPage.push(i)
+        }
+        return listPage;
+    }
 
     useEffect(()=>{
         fetch(`https://api.github.com/users/${props.user}/repos`).then(resp=>resp.json()).then(data=>setRepoArray(data))
     },[props.user])
 
+    const Pagination=(props)=>{
+        const pageList=pages()
+
+        const handlepge=(page)=>{
+            setPage(page)
+            setEndIndex(page*10)
+            setStartindex(page*10-10)
+        }
+
+        return(
+            <div>
+                {pageList.map(item=>{
+                    return (
+                        <div className="pagination" key={item}>
+                            <button onClick={()=>{handlepge(item);}}>{item}</button>
+                        </div>
+                    )
+                })}
+            </div>
+        )
+    }
     const ListView=(props)=>{
       return(
           props.data.slice(startIndex,endIndex).map(repo=>{
               return (
                   <div className="nameLayout">
-                    <h4>{repo.name}</h4>
+                    <h4>
+                        <a href={repo.html_url}>{repo.name}</a>
+                    </h4>
                   </div>
               )
           })
       )
     }
 
-    const handleNextPage=()=>{
-        setPage(page+1)
-        setStartindex(endIndex)
-        setEndIndex(endIndex+10)
+    const handlePagination=(page)=>{
+        setEndIndex(page*10)
+        setStartindex(page*10-10)
+        setPage(page)
     }
-
-    const handlePrevPage=()=>{
-        setPage(page+1)
-        setStartindex(startIndex-10)
-        setEndIndex(endIndex-10)
-    }
-
-
 
     return (
         <div>
@@ -57,8 +78,9 @@ export default function Repo(props) {
             </div>
             {totalNumberRepo!==0?(
             <div className="buttonLayout">
-                {page===1?<button disabled>Previous</button>:<button onClick={handlePrevPage}>Previous</button>}
-                {page===totalPages?<button disabled>Next</button>:<button onClick={handleNextPage}>Next</button>}
+                {page===1?<button disabled>Previous</button>:<button onClick={()=>{handlePagination(page-1)}}>Previous</button>}
+                <Pagination totalPages={totalPages}/>
+                {page===totalPages?<button disabled>Next</button>:<button onClick={()=>{handlePagination(page+1)}}>Next</button>}
             </div>
             ):(
                 <div>
